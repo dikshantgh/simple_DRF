3fe51af920349168c4a631b58f07b545253ea74a
Dikshant Ghimire <dikkughimire@gmail.com>
2020-07-30 22:00:11 +0200

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,9 @@ from books.models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
+#     modules = ModuleSerializer(many=True, read_only=True)
+# 	  subject = serializers.SlugRelatedField(read_only=True, slug_field='title')
+
     class Meta:
         model = Book
         fields = ('title', 'subtitle', 'author', 'isbn',)
